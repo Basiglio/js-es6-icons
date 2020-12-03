357c6e3312c764ba4b42affa8dc766f240dadf5e
Emilo <emilio.furnari87@gmail.com>
2020-12-03 15:14:16 +0100

    milestone 1 fatta

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,4 @@
-// milestone 1:
-// definire un array di oggetti; ogni oggetto rappresenta un'icona, che è caratterizzata da: nome, prefisso, tipo e famiglia.
 
-// Utilizzando la funzione forEach e il template literal, visualizzare in pagina tutte le icone con il proprio nome.
 
 
 
